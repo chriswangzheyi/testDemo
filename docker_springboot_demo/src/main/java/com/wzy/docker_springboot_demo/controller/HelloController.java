191851c94d2d12de1d4d754d2b6c6fe491568b42
chriswangzheyi <chriswangzheyi@hotmail.com>
2019-09-24 13:38:09 +0800

    test

--- docker_springboot_demo/src/main/java/com/wzy/docker_springboot_demo/controller/HelloController.java
+++ docker_springboot_demo/src/main/java/com/wzy/docker_springboot_demo/controller/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @RequestMapping(value = "/hi", method = RequestMethod.GET)
     public String hello() {
-        return "hi 444";
+        return "hi 555";
     }
 
 }
